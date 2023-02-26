f6e925742bc2776d394c184864e6189379888db7
Ellshie <123538471+Ellshie@users.noreply.github.com>
2023-02-26 19:47:25 +0800

    3 files added

---
+++ Express/uploads/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const path = require('path');
+const mime = require('mime-types');
+const multer = require('multer');
+
+const app = express();
+
+const fileStorage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage: fileStorage }).single('myFile');
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + './file_upload.html');
+});
+
+app.post('./uploads', function (req, res) {
+  upload(req, res, function (err) {
+    if (err) {
+      return res.end('Error uploading file.');
+    }
+    res.sendFile(__dirname + '/file_uploaded.html');
+  });
+});
+app.listen(7110, function () {
+  console.log('Server is running');
+});
